876887e8212a027b9d5d855a0e06c68c55ddc1ad
RHamudi <ramonramos.silva16@gmail.com>
2022-11-28 19:31:58 -0300

    .

--- Balta/SubscriptionContext/Student.cs
+++ Balta/SubscriptionContext/Student.cs
@@ -2,14 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Balta.NotificationContext;
 using Balta.SharedContext;
 
 namespace Balta.SubscriptionContext
 {
     public class Student : BaseContent
     {
+        public Student()
+        {
+            Subscriptions = new List<Subscription>();
+        }
         public string Email { get; set; }
         public string Name { get; set; }
         public User User { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+        public void CreateSubscription(Subscription subscription)
+        {
+            if (IsPremium)
+            {
+                AddNotification(new Notification("Premium", "O aluno ja contem uma assinatura ativa!"));
+                return;
+            }
+            Subscriptions.Add(subscription);
+        }
+        public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
     }
 }
